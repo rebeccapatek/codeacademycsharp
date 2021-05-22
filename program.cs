56e7c387a605220b01becade6661df1134a7260b
rebeccapatek <rebeccapatek@gmail.com>
2021-05-22 08:50:11 -0500

    finished string

--- program.cs
+++ program.cs
@@ -10,6 +10,7 @@ namespace codeacademycsharp
             Console.WriteLine();
             Console.WriteLine("Type anything into the console and I'll try to figure out what it is so you can begin to learn datatypes.");
             string strang = Console.ReadLine();
+            Console.WriteLine($"{strang} is a string");
         }
     }
 }
